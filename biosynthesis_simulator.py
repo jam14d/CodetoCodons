917ef86fe291468f547522a7b7fd3168b6e00e75
jmortelrb <jmortel@revealbio.com>
2025-04-02 13:44:55 -0700

    update font for intro

--- biosynthesis_simulator.py
+++ biosynthesis_simulator.py
@@ -110,6 +110,7 @@ def app():
 
         .intro-scroll {
             background-color: #1b1b1b;
+            font-family: "Courier New", Courier, monospace;
             padding: 20px;
             margin: 20px 0;
             font-size: 20px;
