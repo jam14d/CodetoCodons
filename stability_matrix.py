39460775d563914864587a02604fa6126ce485cb
jmortelrb <jmortel@revealbio.com>
2025-04-02 16:05:43 -0700

    changing subtitle to h4

--- stability_matrix.py
+++ stability_matrix.py
@@ -47,7 +47,7 @@ def app():
         <div style="background-color: #0e0e0e; padding: 20px; border-radius: 10px; color: #00ffea; 
                     font-family: 'Orbitron', monospace; text-align: left; font-size: 14px; line-height: 1.5;
                     box-shadow: 0 0 15px #00ffea, 0 0 25px #00ffaa; max-width: 900px; margin: auto; padding: 20px;">
-            <h3>How Can We Measure DNA Stability?</h3>
+            <h4>How Can We Measure DNA Stability?</h4>
             
         GC content—the proportion of guanine (G) and cytosine (C) bases in a DNA sequence—plays a critical role in DNA stability. Higher GC content enhances stability due to the stronger triple hydrogen bonding between G and C pairs, making the DNA more resistant to denaturation.
         </div>
